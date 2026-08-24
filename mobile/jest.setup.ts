ec6ad767545e9e45b3943b7391af789b1e1bd159
Muhamad Iqbal Mamat <muh.iqbal@bciasia.co.id>
2026-08-24 20:30:42 +0700

    feat: add Jest testing infrastructure and implement initial unit tests for Button component

---
+++ mobile/jest.setup.ts
@@ -0,0 +1,5 @@
+import "@testing-library/react-native";
+import "./config/Unistyles";
+
+// Mock Scale utilities if necessary or ensure Dimensions default
+jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
